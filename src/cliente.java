876c05426e45a144dfb77ac7c0ec2e0333e4d0e5
67645 <antonio.sergio.costa.gomes@gmail.com>
2019-04-08 16:14:00 +0100

    esqueleto

--- src/cliente.java
+++ src/cliente.java
@@ -1,8 +1,47 @@
-public class Cliente{
-	int id;
 
-	public Class(int id){
-		this.id = id;
-	}
+public class Cliente
+{
 
+	    class AgenteUDP{
+        	/*
+				Porção de envio e receção de packets e establecimento de conexao.
+        	*/
+
+        }
+           
+        class TransferCC{
+        	/*
+				controlo de packets (fragmentação, montagem, controlo de erros, controlo de receção e envio de mensagens de erro)
+        	*/
+
+        }
+
+	public static void main(String [] argv) throws IOException
+	{
+		//Declaring host and port, initializing ConnectionSocket, WriteBuffer and ReadBuffer;
+		String host = argv[0];
+        int port = Integer.parseInt(argv[1]);
+		Socket cs = new Socket(host, port);        
+        PrintWriter out = new PrintWriter(cs.getOutputStream());
+        BufferedReader in = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+
+
+        while(true){
+            //Reading portion of the client;       
+            
+            if(s == null) break;
+            
+            out.flush();
+
+            //Writing portion of the client;
+           
+            if(s1 == null) break;
+            
+                  
+        }
+            
+        out.close();
+        cs.close();
+    }
 }
+
--- src/packet.java
+++ src/packet.java
@@ -1,7 +1,64 @@
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+
+ id -> id do packet
+
+ size -> tamanho do packet
+
+ more_fragments -> se é o último fragmento ou não
+
+ ioffset -> fragment offset
+
+types
+ 	0 -> connection request
+ 	1 -> data request
+ 	2 -> data packet
+ 	3 -> end of connection
+ 	-1 -> connectio refused
+
+ priority
+ 	0 -> low level
+ 	2 -> mid level
+ 	3 -> max level
+
+filename -> nome do ficheiro a ser enviado/pedido (pode ser null em packets de controlo)
+ 
+data -> ficherio propriamente dito
+
+*/
+
 public class Packet{
 	int id;
+	int size;
+	Bool more_fragments;
+	int offset;
+	int type;
+	int priority;
+	int src_port;
+	int dest_port;
+	int src_id;
+	int dest_id;
+	String filename;
+	File data;
+
 	
-	class Packet(int id){
+	class Packet(int id, int size, int more_fragments, int offset, int type, int priority, int src_port, int dest_port, int src_id; int dest_id; String filename, File data){
 		this.id = id;
+		this.size = size;
+		this.more_fragments = more_fragments;
+		this.offset	= offset;
+		this.type = type;
+		this.priority = priority;
+		this.src_port = src_port;
+		this.dest_port = dest_port;
+		this.src_id = src_id;
+		this.dest_id = dest_id;
+		this.filename = filename;
+		this.data = new File(data);
 	}
 }
