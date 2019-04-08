0ab8a567e1660799a94be7edaff959604ef2a89c
67645 <antonio.sergio.costa.gomes@gmail.com>
2019-04-08 15:07:47 +0100

    classes

---
+++ src/cliente.java
@@ -0,0 +1,8 @@
+public class Cliente{
+	int id;
+
+	public Class(int id){
+		this.id = id;
+	}
+
+}
---
+++ src/servidor.java
@@ -0,0 +1,7 @@
+public class Servidor{
+	int id;
+
+	public Servidor(int id){
+		this.id = id;
+	}
+}
